6cdb55eaf0c5c59e34aa2ce8ea53f5a56f74d3d4
windows10 <2380253499@qq.com>
2018-11-28 08:52:19 +0800

    new file

---
+++ MyProgressLayout/src/main/java/com/github/progress/MyProgressLayout.java
@@ -0,0 +1,22 @@
+package com.github.progress;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/***
+ *   created by zhongrui on 2018/9/21
+ */
+public class MyProgressLayout extends RelativeLayout {
+    public MyProgressLayout(Context context) {
+        super(context);
+    }
+
+    public MyProgressLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyProgressLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
